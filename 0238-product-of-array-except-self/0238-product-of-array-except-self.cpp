027ac2e138884f626d9a797ec9acf28a4b9cdc36
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-07-29 22:30:26 +0530

    Time: 25 ms (78.52%), Space: 27 MB (5.05%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -12,14 +12,13 @@ public:
             pre.push_back(nums[i]*pre[i-1]);
             post.push_back(nums[s-i]*post[i-1]);
         }
-        reverse(post.begin(), post.end());
 
-        nums[0]=post[1];
+        nums[0]=post[s-1];
         nums[s]=pre[s-1];
-        
+
         for(int i=1; i<s; i++)
         {
-            nums[i]=pre[i-1]*post[i+1];
+            nums[i]=pre[i-1]*post[s-i-1];
         }
         return nums;
     }
