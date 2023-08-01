f4c510c438394072bcc7828f2fc8543a9fec991e
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-08-01 19:54:18 +0530

    Time: 13 ms (72.53%), Space: 11.4 MB (12.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -7,17 +7,13 @@ public:
         int i=0,j=nums.size()-1;
         for(int i=0;i<nums.size(); i++)
         {
-            m[nums[i]] = i+1;
-        }
-
-        for(int i=0;i<nums.size(); i++)
-        {
-            if(m[target-nums[i]]>=1 && m[target-nums[i]]!=i+1)
+            if(m[target-nums[i]]>=1)
             {
                 ans.push_back(m[target-nums[i]]-1);
                 ans.push_back(i);
                 return ans;
             }
+            m[nums[i]] = i+1;
         }
         return ans;
     }
