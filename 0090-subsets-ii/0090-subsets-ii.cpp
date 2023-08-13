278980d7104850cf30f27d730392ba7293a1946b
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-08-13 16:24:48 +0530

    Time: 13 ms (10.33%), Space: 18.5 MB (5.14%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    set<vector<int>> ans;
+public:
+    void sub(vector<int> nums , vector<int> v ,int i)
+    {
+        if(i==nums.size())
+        {
+            ans.insert(v);
+            return;
+        }
+
+        v.push_back(nums[i]);
+        sub(nums,v,i+1);
+        v.pop_back();
+        sub(nums,v,i+1);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+        vector<int> v;
+        sort(nums.begin(),nums.end());
+        sub(nums , v , 0);
+        vector<vector<int>> ANS;
+
+        for(auto it : ans)
+        {
+            ANS.push_back(it);
+        }
+        return ANS; 
+    }
+};
