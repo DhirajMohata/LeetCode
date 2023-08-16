85401b2e82f5599b5f7384879662e94358869ffe
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-08-16 18:53:43 +0530

    Time: 249 ms (79.05%), Space: 148.9 MB (24.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,22 +1,61 @@
 class Solution {
 public:
+
     vector<int> maxSlidingWindow(vector<int>& nums, int k) 
     {
+        priority_queue<pair<int, int>> pq;
         vector<int> ans;
-        deque<int> d;
 
-        for(int i=0; i<nums.size(); i++)
+        for(int i=0; i<k; i++)
         {
-            while(!d.empty() && nums[i]>=nums[d.back()])
-                d.pop_back();
-            d.push_back(i);
+            pq.push({nums[i], i});
+        } 
+        
+        ans.push_back(pq.top().first);
 
-            if(d.front()==i-k)
-                d.pop_front();
+        for(int i=k; i<nums.size(); i++)
+        {
+            while(!pq.empty() && pq.top().second<=(i-k))
+                pq.pop();
 
-            if(i>=k-1)
-                ans.push_back(nums[d.front()]);
+            pq.push({nums[i], i});
+            ans.push_back(pq.top().first);
         }
+
         return ans;
     }
 };
+
+// above solution is using sliding window and prioprity queue of pair in which we are storing value and their index in sorted manner
+
+
+
+
+//Using Deque
+
+
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+//     {
+//         vector<int> ans;
+//         deque<int> d;
+
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             while(!d.empty() && nums[i]>=nums[d.back()])
+//                 d.pop_back();
+//             d.push_back(i);
+
+//             if(d.front()==i-k)
+//                 d.pop_front();
+
+//             if(i>=k-1)
+//                 ans.push_back(nums[d.front()]);
+//         }
+//         return ans;
+//     }
+// };
+
+// here first element is always the biggest thats why we are removing all from the back which are smaller then current element
