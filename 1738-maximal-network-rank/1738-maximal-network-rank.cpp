f3658ae3c13450baaff3cb60120840e71e6df06a
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-08-18 08:52:23 +0530

    Time: 158 ms (12.81%), Space: 52.3 MB (5.24%) - LeetHub

---
+++ 1738-maximal-network-rank/1738-maximal-network-rank.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) 
+    {
+        map<pair<int,int>,int> m1;
+        vector<int> m2(n,0);
+        int ans=0;
+
+        for(int i=0; i<roads.size(); i++)
+        {
+            m1[{roads[i][0],roads[i][1]}]++;
+            m2[roads[i][0]]++;
+            m2[roads[i][1]]++;
+        }
+
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                int c = m2[i]+m2[j]; 
+                c-=m1[{i,j}]+m1[{j,i}];
+                ans = max(ans,c);
+            }
+        }
+        return ans;
+    }
+};
