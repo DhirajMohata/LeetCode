f6a2e514a727e4499784736a561c9dd6835bf332
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-01 21:25:45 +0530

    Time: 12 ms (7.87%), Space: 8.6 MB (5.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,10 +3,12 @@ public:
     vector<int> countBits(int n) 
     {
         vector<int> ans;
+        int x, y = 0;
 
         for(int i=0; i<=n; i++)
         {
-            int x = i , y = 0;
+            x = i;
+            y = 0;
             while(x)
             {
                 if(x%2==1)
