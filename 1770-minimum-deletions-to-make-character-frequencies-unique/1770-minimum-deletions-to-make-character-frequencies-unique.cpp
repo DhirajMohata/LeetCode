f85cf335db925356294628f9233174bdc84afcbe
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-12 12:34:34 +0530

    Time: 74 ms (42.72%), Space: 17.5 MB (37.86%) - LeetHub

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minDeletions(string s) 
+    {
+        unordered_map<char,int> m;
+        unordered_map<int,int> freq;
+        int ans = 0;
+
+        for(int i=0; i<s.size(); i++)
+        {
+            m[s[i]]++;
+        }
+
+        for(auto [key,val] : m)
+        {
+            freq[val]++;
+        }
+
+        for(auto [key,val] : m)
+        {
+            if(freq[val]>=2)
+            {
+                int x = val;
+                while(x)
+                {
+                    ans++;
+                    if(freq[x]==0)
+                    {
+                        freq[x]=1;
+                        ans--;
+                        break;
+                    }
+                    x--;
+                }
+                freq[val]--;
+                cout << ans << " " << freq[val] << " " << val << "  ";
+            }
+        }
+
+        return ans;
+    }
+};
