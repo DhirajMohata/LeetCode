6dafc3a603bb5fc7c6d1ae8617eea36d60a698bc
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-13 22:08:11 +0530

    Time: 29 ms (79.17%), Space: 32.4 MB (41.11%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& nums, int l, int x) 
+    {
+        int i=0 , j=1;
+        if(nums.size()==l)
+            return nums;
+        vector<int> ans;
+
+            while(j<nums.size() && nums[j]<x)
+                j++;
+            
+            if(j==nums.size())
+            {
+                for(int k=nums.size()-l; k<nums.size(); k++)
+                    ans.push_back(nums[k]);
+                return ans;
+            }
+
+            i = j-1;
+
+            while(l)
+            {
+                if((i==-1) || (j!=nums.size() && abs(nums[i]-x)>abs(nums[j]-x)))
+                {
+                    ans.push_back(nums[j]);
+                    j++;
+                    l--;
+                }
+                else
+                {
+                    ans.push_back(nums[i]);
+                    i--;
+                    l--;
+                }
+            }
+            
+            if(ans.size()!=0)
+            {
+                sort(ans.begin(),ans.end());
+                return ans;
+            }
+            cout << l << " ";
+            for(int m=0; m<l; m++)
+                ans.push_back(nums[m]);
+            return ans;
+    }
+};
