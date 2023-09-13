87125635a42c7a6bffa8d550fc12351ca412f2f0
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-13 19:00:56 +0530

    Time: 0 ms (100%), Space: 6.5 MB (13.97%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    int integerBreak(int n) {
-        int dp[n][n+1];
-		for(int i=1;i<n;i++)
+    int integerBreak(int n) 
+    {
+        vector<int> dp(n+1,1);
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++)
         {
-			for(int j=0;j<=n;j++)
+            int x = n/i , temp = 1;
+            int y = n%i;
+
+            for(int j=0; j<i; j++)
             {
-                if(i==1)
-                    dp[1][j]=1;
+                if(y)
+                {
+                    temp *= x+1;
+                    y--;
+                }
                 else
-                {
-                    dp[i][j]=dp[i-1][j];
-                    if(j>=i) 
-                        dp[i][j]=max(dp[i][j],i*dp[i][j-i]);
-                }
-			}
-		}
-
-        cout<<"      ";
-        for(int j2=0;j2<=n;j2++)
-            cout<<j2<<" ";
-        cout<<endl;
-        for(int i2=1;i2<n;i2++)
-        {
-            int fl=1;
-            for(int j2=0;j2<=n;j2++)
-            {
-                if(fl)
-                {
-                    cout<<i2<<" --> ";
-                    fl=0;
-                }
-                cout<<dp[i2][j2]<<" ";
+                    temp *= x;
             }
-            cout<<endl;
-        }
 
-
-		return dp[n-1][n];
+            dp[i] = max(dp[i-1], temp);
+        }    
+        return dp[n];
     }
 };
