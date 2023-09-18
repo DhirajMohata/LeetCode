be055c4b1352c46c2bf6209d7bf29ac570886f77
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-18 11:11:06 +0530

    Time: 534 ms (5.27%), Space: 66.3 MB (91.08%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& nums) 
+    {
+        int i=0 , j=1 , x = 0 , y=1 , u = nums[0] , v = nums[0] , ans = 0;
+ 
+        while(j<nums.size() && nums[j]==u)
+            j++;
+
+        if(j<nums.size())
+        {
+            v = nums[j];
+            ans = max(ans,j-i+1);
+        }
+        else
+            ans = max(ans,j-i);
+
+        x = j-1; 
+        y = j;
+
+        j++;
+
+        while(j<nums.size())
+        {
+            if(nums[j]==u || nums[j]==v)
+            {
+                ans = max(ans,j-i+1);
+
+                cout << ans << " " << i << " " << j << " s ";
+                if(nums[j]==u)
+                {
+                    x = j;
+                }
+                else
+                {
+                    y = j;
+                }
+
+                cout << x << " " << y << " x " ;
+
+            }
+            else
+            {
+                if(nums[j-1]==u)
+                {
+                    i = y+1;
+                }
+                else
+                {
+                    i = x+1;
+                    u = v;
+                    x = y;
+                }
+                v = nums[j];
+                y = j;
+            }
+            j++;
+        }
+
+        return ans;
+    }
+};
