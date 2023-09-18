e4940b20f3a9664ca21f82af706eabf5b35fee0f
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-18 08:20:14 +0530

    Time: 10 ms (54.38%), Space: 10.7 MB (13.97%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    bool cmpp(vector<int> a , vector<int> b)
-    {
-        int x=0,y=0;
-        for(int i=0; i<a.size()-1; i++)
-        {
-            if(a[i]==1)
-                x++;
-            if(b[i]==1)
-                y++;
-        }
-        return x<=y;
-    }
-
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
     {
         for(int i=0; i<mat.size(); i++)
@@ -20,7 +7,7 @@ public:
             mat[i].push_back(i);
         }
 
-        sort(mat.begin() , mat.end() );
+        sort(mat.begin() , mat.end());
 
         vector<int> ans;
 
