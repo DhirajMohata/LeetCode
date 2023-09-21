e0af60e524bfd2aab614558e0ef210190441effe
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-21 18:54:26 +0530

    Time: 441 ms (10.57%), Space: 194.1 MB (5.05%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-    int longestSubarrayWithSumX(vector<int>&nums,int target)
+    int longestSubarrayWithSumX(vector<int>&nums,int t)
     {
-        int left=0,right=0;
-        int sum=0;
-        int maxlen=0;
-        while(right<nums.size())
+        unordered_map<int,int> m;
+        int sum = 0;
+        int ans = -1;
+
+        for(int i=0; i<nums.size(); i++)
         {
-            sum+=nums[right];
-            while(sum>target && left<=right)
+            sum += nums[i];
+
+            if(m[sum-t]>=1)
             {
-                sum-=nums[left];
-                left++;
+                ans = max(ans , i - m[sum-t] + 1);
             }
-            if(sum==target)
-                maxlen=max(maxlen,right-left+1);
-            right++;
+
+            if(sum == t)
+                ans = max(ans , i+1);
+
+            m[sum] = i+1;
         }
-        return maxlen;
+
+        return ans;
+        
     }
-    int minOperations(vector<int>& nums, int x) {
-        int sum=0;
-        for(int i=0;i<nums.size();i++)
-            sum+=nums[i];
-        int target=sum-x;
-        if(target==0)
+    int minOperations(vector<int>& nums, int x) 
+    {
+        int sum = 0;
+
+        for(int i=0; i<nums.size(); i++)
+            sum += nums[i];
+
+        int target = sum - x;
+
+        if(sum == x)
             return nums.size();
-        int len=longestSubarrayWithSumX(nums,target);
-        if(len==0 || target<0)
-            return -1;
+
+        int len = longestSubarrayWithSumX( nums , target );
+
+        if(len == -1)
+            return len;
+
         return nums.size()-len;
     }
 };
