1be06085da7be4bdf8b50c2bb37fe51ab19ba6ad
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-21 10:06:09 +0530

    Time: 35 ms (25.48%), Space: 89.6 MB (55.15%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        double ans = 0;
+
+        int i=0 , j = 0 , n = nums1.size() + nums2.size() , x = 0;
+
+        if(n%2==0)
+        {
+            while(i<nums1.size() || j<nums2.size())
+            {
+                x++;
+                if(j>=nums2.size() || (i<nums1.size() && nums1[i]<=nums2[j]))
+                {
+                    i++;
+                    if(x==n/2)
+                    {
+                        ans += nums1[i-1];
+                
+                        if(j>=nums2.size() || (i<nums1.size() && nums1[i]<=nums2[j]))
+                        {
+                            ans += nums1[i];
+                            return ans/2;
+                        }
+                        else
+                        {
+                                ans += nums2[j];
+                                return ans/2;
+                        }
+                    }
+                }
+                else
+                {
+                        j++;
+                        if(x==n/2)
+                        {
+                            ans += nums2[j-1];
+                            if(j>=nums2.size() || (i<nums1.size() && nums1[i]<=nums2[j]))
+                            {
+                                ans += nums1[i];
+                                return ans/2;
+                            }
+                            else
+                            {
+                                    ans += nums2[j];
+                                    return ans/2;
+                            }
+                        }
+                }
+            }
+        }   
+        else
+        {
+             while(i<nums1.size() || j<nums2.size())  
+             {
+                x++;
+                if(j>=nums2.size() || (i<nums1.size() && nums1[i]<=nums2[j]))
+                {
+                    i++;
+                    if(x==(n+1)/2)
+                    {
+                        ans+=nums1[i-1];
+                        return ans;
+                    }
+                }    
+                else
+                {
+                        j++;
+                        if(x==(n+1)/2)
+                        {
+                            ans+=nums2[j-1];
+                            return ans;
+                        }
+                }
+             } 
+        } 
+        return 0;
+    }
+};
