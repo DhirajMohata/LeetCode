496b1bb6ab9c8c6eb161968bbd040eff338aa786
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-25 08:43:23 +0530

    Time: 3 ms (65.73%), Space: 7.1 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) 
+    {
+        unordered_map<char,int> m;
+
+        for(int i=0; i<s.size(); i++)
+        {
+            m[s[i]]++;
+        }    
+
+        for(int i=0; i<t.size(); i++)
+        {
+            if(m[t[i]]==0)
+                return t[i];
+
+            m[t[i]]--;
+        }
+
+        return 'a';
+    }
+};
