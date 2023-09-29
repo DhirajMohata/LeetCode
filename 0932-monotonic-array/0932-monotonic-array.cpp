ff0ed43ad3528a4ffa98f04cfdadc276a35c1b45
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-09-29 08:30:53 +0530

    Time: 132 ms (26.24%), Space: 96.8 MB (22.05%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -6,13 +6,19 @@ public:
         for(int i=1; i<nums.size(); i++)
         {
             if(nums[i]>nums[i-1])
+            {
                 flag = 1;
+                break;
+            }
         }
 
         for(int i=0; i<nums.size()-1; i++)
         {
             if(nums[i]>nums[i+1])
+            {
                 flag1 = 1;
+                break;
+            }
         }
 
         if(flag == 0 || flag1 == 0)
