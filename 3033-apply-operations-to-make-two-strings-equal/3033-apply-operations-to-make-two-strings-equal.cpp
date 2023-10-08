d1da3586b5cd56e635b16a8fa08014b8798435a8
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-10-08 09:45:59 +0530

    Time: 4 ms (100%), Space: 7.3 MB (100%) - LeetHub

---
+++ 3033-apply-operations-to-make-two-strings-equal/3033-apply-operations-to-make-two-strings-equal.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minOperations(string s1, string s2, int x) 
+    {   
+        vector<int> nums;
+        int y = 0;
+        
+        for(int i=0; i<s1.size(); i++)
+        {
+            if(s1[i]!=s2[i])
+                nums.push_back(i);
+        }
+        
+        int z = nums.size();
+        if(z%2==1)
+            return -1;
+        
+        if(nums.size()==0)
+            return 0;
+
+        vector<int> dp(nums.size()+2,10000000);
+        dp[0]=0;
+        
+        for(int i=1; i<=nums.size(); i++)
+        {
+            dp[i] = dp[i-1]+x;
+            if(i>=2)
+                dp[i] = min(dp[i] , dp[i-2] + 2*(nums[i-1] - nums[i-2]));
+        }
+
+        return dp[nums.size()]/2;
+    }
+};
