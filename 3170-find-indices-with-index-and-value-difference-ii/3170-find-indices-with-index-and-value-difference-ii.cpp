45958e274d0548367ca8e1719a0ba95c1989e759
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-10-15 09:59:43 +0530

    Time: 247 ms (20%), Space: 108 MB (10%) - LeetHub

---
+++ 3170-find-indices-with-index-and-value-difference-ii/3170-find-indices-with-index-and-value-difference-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int index, int value) 
+    {
+        set<pair<int,int>> s;
+        
+        for(int i=index; i<nums.size(); i++)
+        {
+            s.insert({nums[i-index],i-index});
+            auto it =  s.begin();
+            auto it1 = s.end();
+            it1--;
+            
+            pair<int,int> x = *it, y=*it1;
+            
+            if(abs(x.first-nums[i])>=value)
+            {
+                cout << endl;
+                return {x.second , i};
+            }
+
+            if(abs(y.first-nums[i])>=value)
+            {
+                cout << endl;
+                return {y.second , i};
+            }
+        }
+        cout << endl;
+        return {-1,-1};
+    }
+};
