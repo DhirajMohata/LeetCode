2a310f1b293c14b0a3d22fa1c0f95a61bf0e2a34
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-10-22 10:12:38 +0530

    Time: 196 ms (53.85%), Space: 128.9 MB (30.77%) - LeetHub

---
+++ 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+    int ispossible(int x , vector<int>& temp)
+    {
+        int flag = 0 , flag1 = 0;
+        
+        int ans = 0;
+        
+        for(int i=0; i<temp.size(); i++)
+        {
+            int y = temp[i]/x , z = temp[i]%x;
+            
+            if(z<=y)
+                ans += y;
+            
+            else
+            {
+                flag = 1;
+                break;
+            }
+        }
+        
+        if(flag == 1)
+        {
+            ans = 0;
+            for(int i=0; i<temp.size(); i++)
+            {
+                int y = temp[i]/x , z = temp[i]%x;
+                
+                if(z == 0)
+                    ans += y;
+                else if( x - z <= y + 1 || z == x-1)
+                    ans += y + 1;
+
+                else
+                {
+                    flag1 = 1;
+                    break;
+                }
+            }
+        }
+        
+        if(flag == 1 && flag1 == 1)
+        {
+            cout << x;
+            return -1;
+        }
+        return ans;
+    }
+    
+    int minGroupsForValidAssignment(vector<int>& nums) 
+    {
+        unordered_map<int,int> m;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            m[nums[i]]++;
+        }
+        
+        int mini = INT_MAX;
+        vector<int> temp;
+        
+        for(auto [key,val] : m)
+        {
+            mini = min(mini , val);
+            temp.push_back(val);
+        }
+        
+        int ans = INT_MAX;
+        for(int i=1; i<=mini + 1; i++)
+        {
+            int te = ispossible(i , temp);
+
+            if(te != -1)
+            {
+                ans = min(te,ans);
+            }
+        }
+        
+        if(ans == INT_MAX)
+            return -1;
+        
+        return ans;
+    }
+};
