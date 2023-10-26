30637b548d199d17b158cd1fc2dc54eea0dd0d05
Dhiraj Mohata <97870663+DhirajMohata@users.noreply.github.com>
2023-10-26 16:10:24 +0530

    Time: 1322 ms (5.12%), Space: 20.1 MB (5.12%) - LeetHub

--- 0843-binary-trees-with-factors/0843-binary-trees-with-factors.cpp
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.cpp
@@ -4,6 +4,8 @@ public:
     {
         sort(arr.begin(),arr.end());
         unordered_map<int,long long> m;
+        
+        int ans = 0;
 
         for(auto it : arr)
             m[it] = 1;
@@ -12,22 +14,16 @@ public:
         {
             for(int j=2; j<=sqrt(arr[i]); j++)
             {
-                int y = arr[i]/j;
-
-                if(arr[i]%j == 0 && m[j] >= 1 && m[y] >= 1)
+                if(arr[i]%j == 0 && m[j] >= 1 && m[arr[i]/j] >= 1)
                 {
-                    if(j!=y)
-                        m[arr[i]] += 2 * m[j] * m[y];
+                    if(j!=arr[i]/j)
+                        m[arr[i]] += 2 * m[j] * m[arr[i]/j];
                     else
-                        m[arr[i]] += m[j] * m[y];
+                        m[arr[i]] += m[j] * m[arr[i]/j];
                 }
             }
+            ans = (ans + m[arr[i]])%1000000007;
         }
-
-        int ans = 0;
-
-        for(auto [key,it] : m)
-            ans = (ans + it)%1000000007;
 
         return ans;
     }
